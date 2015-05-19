a7c67058c002cfe9234434fb712102b7f44bf7ce
Matt Furtado <mfurtado@hubspot.com>
2015-05-18 22:42:03 -0400

    Add Facebook links.

--- sub_top.php
+++ sub_top.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -7,11 +8,26 @@
 </head>
 
 <body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=211194202264976";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <div id="wrapper">
 
 <div id="header">
-	<img src="../images/logo.gif" alt="stoneways_logo" width="391" height="159" align="left" />
-	
+	<a href="/">
+		<img src="../images/logo.gif" alt="stoneways_logo" width="391" height="159" align="left" />
+	</a>
+
+	<a class="fb-link" href="https://www.facebook.com/stonewayspaving" target="_blank">
+		<img src="../images/facebook_64.png" width="24">
+		Like us on Facebook
+	</a>
+
 	<ul id="nav">
 		<li><a href="../index.php">About</a></li>
 		<li><a href="../process.php">Process</a></li>
@@ -25,10 +41,13 @@
 
 <div class="both"></div>
 
-<div id="main">	
+<div id="main">
 	<div class="left">
 		<script src="../sub_left.js" type="text/javascript"></script>
+		<div class="fb-like" data-href="https://www.facebook.com/stonewayspaving" data-width="230" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
 	</div>
-	
+
 	<div class="right">
 		<div class="br">
+
+
