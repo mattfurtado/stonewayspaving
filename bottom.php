3db2f45e453099ddcb350fac9ac9eb9d19cd3cc3
Matt Furtado <mptado@gmail.com>
2016-09-08 11:25:37 -0400

    Update email in footer

--- bottom.php
+++ bottom.php
@@ -1,1 +1,19 @@
-<div class="both"></div><div id="footer">	<div class="left">		Copyright &copy; 2016 StoneWays Paving, Inc. - <a href="mailto:c-harp@hotmail.com">Email Us</a> - <a href="/privacy-policy.php">Privacy Policy</a>	</div>	<div class="right"></div></div></div><script type="text/javascript">var bbbprotocol = ( ("https:" == document.location.protocol) ? "https://" : "http://" ); document.write(unescape("%3Cscript src='" + bbbprotocol + 'seal-central-westernma.bbb.org' + unescape('%2Flogo%2Fstoneways-285058.js') + "' type='text/javascript'%3E%3C/script%3E"));</script><script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script><script type="text/javascript">_uacct = "UA-3598795-5";urchinTracker();</script></body></html>
+<div class="both"></div>
+
+<div id="footer">
+	<div class="left">
+		Copyright &copy; 2016 StoneWays Paving, Inc. - <a href="mailto:charper42368@yahoo.com">Email Us</a> - <a href="/privacy-policy.php">Privacy Policy</a>
+	</div>
+	<div class="right"></div>
+</div>
+
+</div>
+<script type="text/javascript">var bbbprotocol = ( ("https:" == document.location.protocol) ? "https://" : "http://" ); document.write(unescape("%3Cscript src='" + bbbprotocol + 'seal-central-westernma.bbb.org' + unescape('%2Flogo%2Fstoneways-285058.js') + "' type='text/javascript'%3E%3C/script%3E"));</script>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-3598795-5";
+urchinTracker();
+</script>
+</body>
+</html>
